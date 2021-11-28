18a41d2338d776f45b671b4a72c29db1352f5971
Vinu-01 <95160603+Vinu-01@users.noreply.github.com>
2021-11-28 17:03:17 +0530

    Create First Prog.cpp

---
+++ First Prog.cpp
@@ -0,0 +1,8 @@
+#include<iostream>                       //Including input/output libraries
+using namespace std;
+
+int main()                              //Creating main function
+{
+  cout<<"Hello World, This Is My First Projct";       //Using cout keyword to print output
+  return 0;                                           //Exiting main function
+}
