c25d15d9da29ec683395f68af7c0110157828a13
Vinu-01 <95160603+Vinu-01@users.noreply.github.com>
2021-12-04 09:28:42 +0530

    Adding one line to program

--- First Prog.cpp
+++ First Prog.cpp
@@ -3,6 +3,7 @@ using namespace std;
 
 int main()                              //Creating main function
 {
-  cout<<"Hello World, This Is My First Projct";       //Using cout keyword to print output
+  cout<<"Hello World, This Is My First Projct";
+  cout<<"Trying Hard to learn Git and GitHub";       //Using cout keyword to print output
   return 0;                                           //Exiting main function
 }
