437625281f2f5e7e684292c408700baa21bfd04f
Vinu-01 <95160603+Vinu-01@users.noreply.github.com>
2021-12-06 14:47:03 +0530

    Adding once again the calculations prog.

---
+++ Add.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(void)
+{
+    int x,y,z;
+    cout<<"Enter first number : \n";
+    cin>>x;
+    cout<<"Enter second number : \n";
+    cin>>y;
+    z=x+y;
+    cout<<"Addition of entered two numbers is : "<<z<<"\n";
+    z=x-y;
+    cout<<"Substraction of entered two numbers is : "<<z<<"\n";
+    z=x*y;
+    cout<<"Multiplication of entered two numbers is : "<<z<<"\n";
+    z=x/y;
+    cout<<"Division of entered two numbers is : "<<z<<"\n";
+} 
