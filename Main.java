374b0a6c0197a43ab0260fe991ee055103b6e5c6
Vinu-01 <95160603+Vinu-01@users.noreply.github.com>
2021-12-08 17:46:38 +0530

    Adding .java file

---
+++ Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("This World is just Hell !!");
+	// write your code here
+    }
+}
